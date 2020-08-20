f80a94be1481cd55f17052ec53104b1c3b27315c
Mashamole <Marcus.Ashamole12@myhunter.cuny.edu>
2020-08-19 23:14:04 -0400

    updated testCode.py

--- testCode.py
+++ testCode.py
@@ -2,6 +2,11 @@
 (Tasks)
 
 1. simulate modified DFS algorithm using file path as Index
+    -implement backtracing file path (reverting back to previous path)
+
+[note]
+    - will have to find a way to add visited technique for dirs already visited
+        in order to backtrack
 
 """
 from collections import defaultdict
@@ -9,12 +14,15 @@ import os
 
 graph = defaultdict(list)
 graph_ = defaultdict(list)
+visited = defaultdict(bool)
+searched = defaultdict(bool)
 # Test data for directories and file path
 graph["C:/"].append("Apps")
 graph["C:/"].append("test1.py")
 graph["C:/Apps"].append("dirFile")
 graph["C:/Apps"].append("apptest.cpp")
 graph["C:/Apps/dirFile"].append("word.dox")
+graph["C:/Apps/dirFile"].append("word2.dox")
 
 # graph[2].append(10)
 
@@ -22,6 +30,11 @@ graph["C:/Apps/dirFile"].append("word.dox")
 
 
 def testSrchFile(fname, srch_path):
+    visit = defaultdict(bool)
+    return testSrchFileUtil(fname, srch_path, visit)
+
+
+def testSrchFileUtil(fname, srch_path, visited):
     f = os.chdir(srch_path)
     delimiter = '/'
     # # joins path using '/' to create the path
@@ -31,38 +44,77 @@ def testSrchFile(fname, srch_path):
     trackPath.append(srch_path)
     # change idx to string type (use "C:/" as key for graph)
     dirIdx = srch_path
+    visited[dirIdx] = True  # graph for visited paths
     # add srch_path argument to graph
-
     graph_[dirIdx] = arry_result
     print(graph_[dirIdx])
     # check if file is in the first srch_path run
 
-    if fname in graph_[dirIdx]:
+    if fname in graph_[dirIdx]:  # checks if file is in root directory
         print("file exist")
         keys = graph_.keys()
+        # print(keys)
+        # print(srch_path)
         # assign the key path for path concatenation in next lines
         keyPath = srch_path if srch_path in keys else " "
+        print(keyPath)
         if keyPath[len(keyPath)-1] != '/':
+            print("did run 1st return")
+            print(keyPath + '/' + fname)
             return keyPath + '/' + fname
         else:
+            print("did run 2nd return ")
+            print(keyPath + fname)
             return keyPath + fname
-    for f in arry_result:
+
+    for f in arry_result:  # traverse through list of files in path
         if f[0] != '$':
-            if os.path.isdir(f):  # check
-                pass
-                recurPath = srch_path + delimiter + f
-                testSrchFile(fname, recurPath)
+            # check if file is a dir
+            if os.path.isdir(f):
+                # pass
+                if srch_path[len(srch_path)-1] != '/':  # add delimiter for paths
+                    recurPath = srch_path + delimiter + f
+                else:
+                    recurPath = srch_path + f
+                print(recurPath)
+
+                # find way to backtrack dirs
+                return testSrchFileUtil(fname, recurPath, visited)
+
+            if graph_[dirIdx] == [] or visited[dirIdx] != True:
+                print(f)
+                p = srch_path.split("/")
+                p.pop()
+                pth = p.join()
+                return testSrchFileUtil(fname, pth, visited)
+
                 # graph[dirIdx]
 
                 # os.chdir()
 
 
-x = testSrchFile("Apps", "C:/")
-print(x)
+pth = testSrchFile("AUMIDs", "C:/")
+print(type(pth))
+
+
+def revertDir(pth):
+    delim = "/"
+    new_pth = pth.split("/")
+    new_pth.pop()
+    pth = delim.join(new_pth)
+    print(pth)
+    return pth
+
 
 # v = "C:/Apps/dirFile"
+# x = revertDir(v)
+# os.chdir(x)
+# print(os.listdir())
+# searched[v] = False
 # fname = "word.dox"
 # keys = graph.keys()
+# if searched[v] == False:
+#     print("false")
 # # assign the key path for path concatenation in next lines
 # keyPath = v if v in keys else " "
 # for i in graph[v]:
